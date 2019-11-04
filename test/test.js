1e2089ce90079b632c350d72885e686d5685f781
Jeewaka Fernando <jefferdomd@gmail.com>
2019-11-04 20:41:45 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,46 +1,23 @@
-(function ($) {
+$(document).ready(function () {
+    function hasMatch(JSON, key, value) {
+        var hasMatch = false;
+        for (var index = 0; index < JSON.length; ++index) {
+
+            var line = JSON[index];
+
+            if (line[key] == value) {
+                hasMatch = true;
+                break;
+            }
+        }
+        return hasMatch;
+    }
     //var qty = Math.round(5.4); 
     $("#AddMatNB").on("change", function (e) {
 
         var data = {
             "PO": [
             ]
-        };
-        var data2 =
-        {
-            "PONO": "4100017583",
-            "Customer": "Jack Wills",
-            "Style": "JACKW193F-022",
-            "Product": "Pant",
-            "Color": {
-                "Black": {
-                    "10 Yrs": "297",
-                    "12 Yrs": "186",
-                    "14 yrs": "61",
-                    "16 Yrs": "19",
-                    "4 Yrs": "34",
-                    "6 Yrs": "134",
-                    "8 Yrs": "269"
-                },
-                "Charcol": {
-                    "10 Yrs": "445",
-                    "12 Yrs": "280",
-                    "14 yrs": "92",
-                    "16 Yrs": "28",
-                    "4 Yrs": "51",
-                    "6 Yrs": "201",
-                    "8 Yrs": "403"
-                },
-                "Navy": {
-                    "10 Yrs": "445",
-                    "12 Yrs": "280",
-                    "14 yrs": "92",
-                    "16 Yrs": "28",
-                    "4 Yrs": "51",
-                    "6 Yrs": "201",
-                    "8 Yrs": "403"
-                }
-            }
         };
 
         var ext = $("#AddMatNB").val().split(".").pop().toLowerCase();
@@ -53,65 +30,76 @@
             )
             return false;
         }
-        let pos = data['PO'];
-        pos.push(data2);
-        console.log(pos);
-
-        //console.log(pos[0].PONO);
 
         if (e.target.files != undefined) {
             var reader = new FileReader();
             reader.onload = function (e) {
-                csvResult = e.target.result.split("\n");
-                csvResult.forEach(function (element, i) {
-                    if (i !== 0) {
-                        var row = element.split(",");
-                        var pono = row[1];
+                csvResult = $.csv.toObjects(e.target.result);
+                csvResult.forEach(function (row, i) {
 
-                        var colorName = row[4];
-                        var sizeName = row[5];
+                    var ponoCSV = row.CustomerPONumber;
 
-                        for (let index in pos) {
-                            const po = pos[index];
-                            if (po.PONO === pono) {
-                                console.log("Found: " + pono);
-                                if (po.Color.hasOwnProperty(row[4])) {
-                                    console.log("Color found");
-                                    let size = {
-                                        sizeName: Math.round(row[7])
-                                    }
-                                    po.Color[colorName].push(size);
+                    var colorNameCSV = row.color;
+                    var sizeNameCSV = row.size;
+                    var CutQtyCSV = Math.round(row.CutQty).toString();
+
+                    if (data.PO.length != 0) {
+                        for (let index in data.PO) {
+                            //console.log(data.PO[index]);
+                            if (data.PO[index].PONO === ponoCSV) {
+
+                                //console.log("Found: " + ponoCSV);
+                                if (data.PO[index].Color.hasOwnProperty(row.color)) {
+
+                                    //console.log("Color found " + row.color);
+
+                                    data.PO[index].Color[colorNameCSV][sizeNameCSV] = CutQtyCSV;
+
                                 } else {
-                                    console.log("New color");
-                                    let color = {
-                                        colorName: {
-                                            sizeName: Math.round(row[7])
-                                        }
-                                    }
-                                    po.Color.push(color);
+
+                                    //console.log("New color " + row.color);
+
+                                    let color = JSON.parse('{"' + colorNameCSV + '": {"' + sizeNameCSV + '":"' + CutQtyCSV + '"}}');
+                                    data.PO[index].Color = color;
+
                                 }
                             }
                             else {
-                                let po =
-                                {
-                                    "PONO": row[1],
-                                    "Customer": row[0],
-                                    "Style": row[2],
-                                    "Product": row[3],
-                                    "Color": {
-                                        colorName: {
-                                            sizeName: Math.round(row[7])
-                                        }
-                                    }
-                                };
+                                if (!hasMatch(data.PO, "PONO", ponoCSV)) {
+                                    //console.log("New PO: " + row.CustomerPONumber);
+                                    let ponew2 =
+                                    {
+                                        "PONO": row.CustomerPONumber,
+                                        "Customer": row.customer,
+                                        "Style": row.StyleNumber,
+                                        "Product": row.product,
+                                        "Color": {}
+                                    };
+                                    let color = JSON.parse('{"' + colorNameCSV + '": {"' + sizeNameCSV + '":"' + CutQtyCSV + '"}}');
+                                    ponew2.Color = color;
+                                    data['PO'].push(ponew2);
+                                }
+
                             }
                         }
+                    } else {
+                        //console.log("New PO: " + row.CustomerPONumber);
+                        let ponew1 =
+                        {
+                            "PONO": row.CustomerPONumber,
+                            "Customer": row.customer,
+                            "Style": row.StyleNumber,
+                            "Product": row.product,
+                            "Color": {}
+                        };
+                        let color = JSON.parse('{"' + colorNameCSV + '": {"' + sizeNameCSV + '":"' + CutQtyCSV + '"}}');
+                        ponew1.Color = color;
+                        data['PO'].push(ponew1);
                     }
                 });
+                console.log(data.PO);
             }
             reader.readAsText(e.target.files.item(0));
         }
-
     });
-
-})(jQuery);
+});
