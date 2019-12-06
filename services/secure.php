6fd4a0ce62f8f58cac57c701fc2da238309de2b5
Jeewaka Fernando <jeewakaphotography@gmail.com>
2019-12-06 18:09:30 +0530

    upadte

--- services/secure.php
+++ services/secure.php
@@ -1,50 +1,53 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
-error_reporting(0);
-include_once($_SERVER['DOCUMENT_ROOT'] . '/services/database.php');
-include_once($_SERVER['DOCUMENT_ROOT'] . '/services/token.php');
-
-class Secure { // Secure The login
-
+goto S6UaX;
+f5Qpb: ini_set("\144\151\163\x70\x6c\x61\x79\137\145\162\162\x6f\x72\163", 1);
+goto aZJAD;
+S6UaX: error_reporting(E_ALL);
+goto f5Qpb;
+GNCJa: include_once $_SERVER["\104\117\x43\125\x4d\105\116\124\137\x52\117\x4f\124"] . "\x2f\163\145\162\x76\151\x63\x65\163\57\144\141\x74\x61\142\x61\163\145\56\x70\150\x70";
+goto W3Bza;
+W3Bza: include_once $_SERVER["\x44\117\103\125\115\105\116\x54\x5f\122\117\117\x54"] . "\57\163\x65\162\166\x69\143\x65\x73\57\164\157\x6b\145\x6e\56\160\150\x70";
+goto bNIhQ;
+bNIhQ: class Secure
+{
     public function secureLog()
     {
         $stat = 0;
         $db = new Database();
-        if (isset($_COOKIE['uid']) && isset($_COOKIE['key'])) {
+        if (isset($_COOKIE["\165\151\x64"]) && isset($_COOKIE["\x6b\145\171"])) {
             $stat = 1;
-            $query = "select * from umf where uid='{$_COOKIE['uid']}'";
+            $query = "\x73\145\154\x65\x63\164\x20\x2a\40\146\162\x6f\155\x20\x75\x6d\146\40\x77\150\145\x72\145\x20\x75\x69\144\x3d\x27{$_COOKIE["\x75\151\x64"]}\47";
             if ($result = $db->select($query)) {
                 while ($row = mysqli_fetch_assoc($result)) {
-                    if($_COOKIE['key'] == $row['keyR']){
+                    if ($_COOKIE["\153\145\171"] == $row["\153\x65\x79\x52"]) {
                         $stat = 1;
-                    }
-                    else {
+                    } else {
                         $this->logout();
                         $stat = 0;
                     }
                 }
             }
-        }
-        else {
+        } else {
             $stat = 0;
             $this->logout();
         }
-    
         return $stat;
     }
-
     public function logout()
     {
-        if (isset($_SERVER['HTTP_COOKIE'])) {
-            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+        if (isset($_SERVER["\110\x54\124\x50\137\103\117\x4f\x4b\x49\105"])) {
+            $cookies = explode("\x3b", $_SERVER["\x48\124\x54\x50\137\103\117\117\113\111\x45"]);
             foreach ($cookies as $cookie) {
-                $parts = explode('=', $cookie);
+                $parts = explode("\x3d", $cookie);
                 $name = trim($parts[0]);
                 setcookie($name, '', time() - 1000);
-                setcookie($name, '', time() - 1000, '/');
-                header("Location:" . '/' . "cms/");
+                setcookie($name, '', time() - 1000, "\x2f");
+                header("\114\x6f\x63\x61\164\151\157\156\72" . "\57" . "\143\x6d\x73\57");
             }
         }
     }
 }
-?>
+goto J3y0K;
+aZJAD: error_reporting(0);
+goto GNCJa;
+J3y0K:
--- services/token.php
+++ services/token.php
@@ -1,65 +1,55 @@
 <?php
 
-//require $_SERVER['DOCUMENT_ROOT']."/vender/autoload.php";
-
 use Picqer\Barcode\BarcodeGeneratorHTML;
 
 class Token
 {
     public static function sh1salt($pwd)
     {
-        $postPwd = "";
+        $postPwd = '';
         $pat = array(2, 3, 5);
         for ($i = 0; $i < strlen($pwd); $i++) {
             $postPwd .= $pwd[$i];
             for ($j = 0; $j < sizeof($pat); $j++) {
                 $asciiE = ord($pwd[$i]) * ord($pwd[$i]);
                 if ($asciiE % $pat[$j] == 0) {
-                    $postPwd .= "&j4s";
+                    $postPwd .= "\46\152\64\x73";
                 }
             }
         }
         $postPwd = sha1($postPwd);
         return $postPwd;
     }
-
     public static function getToken($length)
     {
-        $token = "";
-        $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
-        $codeAlphabet .= "0123456789";
-        $max = strlen($codeAlphabet); // edited
-
+        $token = '';
+        $codeAlphabet = "\x41\x42\103\x44\x45\106\x47\110\111\112\x4b\114\115\x4e\117\x50\121\122\x53\x54\x55\126\127\130\x59\132";
+        $codeAlphabet .= "\141\x62\143\x64\x65\x66\x67\150\x69\152\x6b\x6c\155\156\157\160\x71\162\x73\x74\x75\166\167\170\171\172";
+        $codeAlphabet .= "\x30\61\62\x33\64\65\66\x37\70\71";
+        $max = strlen($codeAlphabet);
         for ($i = 0; $i < $length; $i++) {
             $token .= $codeAlphabet[random_int(0, $max - 1)];
         }
-
         return $token;
     }
-
     public static function getTokenNoOnly($length)
     {
-        $token = "";
-        $codeAlphabet = "0123456789";
-        $max = strlen($codeAlphabet); // edited
-
+        $token = '';
+        $codeAlphabet = "\60\61\62\63\64\65\x36\67\x38\71";
+        $max = strlen($codeAlphabet);
         for ($i = 0; $i < $length; $i++) {
             $token .= $codeAlphabet[random_int(0, $max - 1)];
         }
-
         return $token;
     }
-
     public static function setcsrfk()
     {
         if (!isset($_SESSION)) {
             session_start();
         }
-        $_SESSION['csrfk'] = Token::getToken(100);
-        return $_SESSION['csrfk'];
+        $_SESSION["\143\x73\162\146\153"] = Token::getToken(100);
+        return $_SESSION["\143\x73\162\x66\153"];
     }
-
     public static function chkcsrfk($key)
     {
         if (!isset($_SESSION)) {
@@ -67,9 +57,9 @@ class Token
                 session_start();
             }
         }
-        if (isset($_SESSION['csrfk'])) {
-            if ($_SESSION['csrfk'] == $key) {
-                unset($_SESSION["csrfk"]);
+        if (isset($_SESSION["\143\163\162\x66\x6b"])) {
+            if ($_SESSION["\143\163\x72\146\x6b"] == $key) {
+                unset($_SESSION["\x63\163\x72\146\153"]);
                 return 1;
             } else {
                 return 0;
@@ -78,7 +68,6 @@ class Token
             return 0;
         }
     }
-
     public static function getBarcode($code)
     {
         $bar = new BarcodeGeneratorHTML();
