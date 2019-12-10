1fc9cb4a3269ead5cd1cd3422c6338c646a6a847
Jeewaka Fernando <jeewakaphotography@gmail.com>
2019-12-10 09:45:13 +0530

    Update CreateGateHome.blade.php

--- views/CreateGateHome.blade.php
+++ views/CreateGateHome.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box">
-                        <h4 class="page-title">Create New Gate Pass: {{ $id }}</h4>
+                        <h4 class="page-title">View / Edit Gate Pass: {{ $id }}</h4>
                         <input type="hidden" name="id" value="{{ $id }}">
                     </div>
                 </div>
